ad02662d6e45bb210e7bea80267695239877e343
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-22 10:30:54 +0900

    Set up GraphQL. testField can be retrieved so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: '/graphql'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+  post '/graphql', to: 'graphql#execute'
 end
