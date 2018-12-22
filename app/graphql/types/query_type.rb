3a2c6490cb033404733e7fd07a6d333f2ffa0130
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-22 17:42:56 +0900

    Make it able to retrieve one specific post

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,12 @@ module Types
     def posts
       Post.all
     end
+
+    field :post, Types::PostType, null: false do
+      argument :id, Int, required: false
+    end
+    def post(id:)
+      Post.find(id)
+    end
   end
 end
