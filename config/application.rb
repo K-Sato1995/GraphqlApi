1906105aeca1db876d6297fc450a76d3d01b4c6a
Katsuki <Katsuki_Sato0130@icloud.com>
2019-03-08 19:56:55 +0900

    Add rack-cors for development

--- config/application.rb
+++ config/application.rb
@@ -19,16 +19,14 @@ Bundler.require(*Rails.groups)
 
 module GraphqlApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins [/localhost\:\d+/]
+        resource '*', headers: :any, methods: %i[post]
+      end
+    end
+
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
