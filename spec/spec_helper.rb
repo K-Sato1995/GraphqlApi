9169e2fc30f8dfe3e77e903eb44881bdc0d034e4
Katsuki <Katsuki_Sato0130@icloud.com>
2019-08-28 20:58:47 +0900

    Setup factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
