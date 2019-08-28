afc7f048aa4ad50d54b29f44cb45a3ead4064e2d
Katsuki <Katsuki_Sato0130@icloud.com>
2019-08-28 21:14:18 +0900

    Add tests for mutations

---
+++ spec/graphql/mutations/posts/create_post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::PostMutations::CreatePost do
+  subject(:mutation) { described_class.new(object: nil, context: nil) }
+  let(:attributes) { attributes_for(:post, title: Faker::App.name) }
+
+  describe '#resolve' do
+    subject { mutation.resolve(attributes) }
+    it 'creates a Post' do
+      expect(subject[:post].title).to eq attributes[:title]
+    end
+  end
+end
