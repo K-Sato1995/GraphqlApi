dfaa57996b5621613700841fc464fe28f1f28c9a
Katsuki <Katsuki_Sato0130@icloud.com>
2019-10-09 20:24:09 +0900

    Add enum types

---
+++ app/graphql/types/post_status.rb
@@ -0,0 +1,5 @@
+class Types::PostStatus < Types::BaseEnum
+  value "PUBLISHED", value: 0
+  value "PRIVATE", value: 1
+  value "DRAFT", value: 2
+end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,10 +1,10 @@
 module Types
   class PostType < Types::BaseObject
     description 'Post'
-
     field :id, Int, null: false
     field :title, String, null: false
     field :description, String, null: false
+    field :status, Types::PostStatus, null: false
     field :comments, [Types::CommentType], null: false
   end
 end
