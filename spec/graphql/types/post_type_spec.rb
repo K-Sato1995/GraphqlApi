05a479652532d6b40d23dac96749d4012727a2d5
Katsuki <Katsuki_Sato0130@icloud.com>
2019-10-09 20:27:10 +0900

    Add test for enum

--- spec/graphql/types/post_type_spec.rb
+++ spec/graphql/types/post_type_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Types::PostType do
     example { expect(subject['id'].type.to_graphql.to_s).to eq 'Int!' }
     example { expect(subject['title'].type.to_graphql.to_s).to eq 'String!' }
     example { expect(subject['description'].type.to_graphql.to_s).to eq 'String!' }
+    example { expect(subject['status'].type.to_graphql.to_s).to eq 'PostStatus!' }
     example { expect(subject['comments'].type.to_graphql.to_s).to eq '[Comment!]!' }
   end
 end
